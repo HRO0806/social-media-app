7cdbfc76196813a7e92a53bbfb0c7b4c06ed6217
Henry <henryolson@icloud.com>
2022-10-05 15:11:29 -0500

    Got react router sort of working

---
+++ client/src/components/Game/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Game.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Game() {
+  return(
+    <div label="game">
+      <h1>Game</h1>
+    </div>
+  );
+}
+
+export default Game;
---
+++ client/src/components/Landing/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Landing.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Landing() {
+  return(
+    <div label="landing">
+        <h1>Landing</h1>
+    </div>
+  );
+}
+
+export default Landing;
